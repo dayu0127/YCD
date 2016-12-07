31f48e0dc2f7f418c8220cd76a5b95abfd68cbb5
liyu <liyu1686868@163.com>
2016-12-07 19:01:06 +0800

    导入友盟推送和分享库

--- Podfile
+++ Podfile
@@ -7,5 +7,9 @@ pod 'SDWebImage', '~>3.8'
 pod 'SDCycleScrollView', '~> 1.64'
 pod 'DKNightVersion'
 pod 'JCAlertView'
+pod 'UMengUShare/UI'
+pod 'UMengUShare/Social/Sina'
+pod 'UMengUShare/Social/WeChat'
+pod 'UMengUShare/Social/QQ'
 
 end
--- Pods/Target Support Files/Pods-YCD/Pods-YCD-resources.sh
+++ Pods/Target Support Files/Pods-YCD/Pods-YCD-resources.sh
@@ -76,10 +76,16 @@ EOM
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "DKNightVersion/DKNightVersion/ColorTable/DKColorTable.txt"
   install_resource "JCAlertView/JCAlertView/JCAlertView.bundle"
+  install_resource "UMengUShare/UShareSDK/SocialLibraries/QQ/TencentOpenApi_IOS_Bundle.bundle"
+  install_resource "UMengUShare/UShareSDK/SocialLibraries/Sina/WeiboSDK.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "DKNightVersion/DKNightVersion/ColorTable/DKColorTable.txt"
   install_resource "JCAlertView/JCAlertView/JCAlertView.bundle"
+  install_resource "UMengUShare/UShareSDK/SocialLibraries/QQ/TencentOpenApi_IOS_Bundle.bundle"
+  install_resource "UMengUShare/UShareSDK/SocialLibraries/Sina/WeiboSDK.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
