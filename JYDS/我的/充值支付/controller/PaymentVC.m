1b16c0c575b937718da15aec3542268b0ec3d7db
liyu <liyu1686868@163.com>
2017-02-22 10:56:07 +0800

    alipay add

--- JYDS/我的/充值支付/controller/PaymentVC.m
+++ JYDS/我的/充值支付/controller/PaymentVC.m
@@ -9,7 +9,6 @@
 #import "PaymentVC.h"
 #import "PaymentCell.h"
 #import <AlipaySDK/AlipaySDK.h>
-#import "Order.h"
 #import "DataSigner.h"
 #import "WXApi.h"
 @interface PaymentVC ()<UITableViewDataSource,UITableViewDelegate>
@@ -50,109 +49,52 @@
     cell.selectedBackgroundView.dk_backgroundColorPicker = DKColorPickerWithColors(D_CELL_SELT,N_CELL_SELT,RED);
     return cell;
 }
-- (NSString *)generateTradeNO
-{
-    static int kNumber = 15;
-    
-    NSString *sourceStr = @"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    NSMutableString *resultStr = [[NSMutableString alloc] init];
-    srand((unsigned)time(0));
-    for (int i = 0; i < kNumber; i++)
-    {
-        unsigned index = rand() % [sourceStr length];
-        NSString *oneStr = [sourceStr substringWithRange:NSMakeRange(index, 1)];
-        [resultStr appendString:oneStr];
-    }
-    return resultStr;
-}
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+     [YHHud showWithStatus:@"准备支付"];
     if (indexPath.row == 0) {
     //支付宝支付
-        //将商品信息赋予AlixPayOrder的成员变量
-        Order* order = [Order new];
-        
-        // NOTE: app_id设置
-        order.app_id = @"2017011805188573";
-        
-        // NOTE: 商品数据
-        order.biz_content = [BizContent new];
-        order.biz_content.body = @"我是测试数据";
-        order.biz_content.subject = @"1";
-        order.biz_content.out_trade_no = [self generateTradeNO]; //订单ID（由商家自行制定）
-        order.biz_content.timeout_express = @"30m"; //超时时间设置
-        order.biz_content.total_amount = [NSString stringWithFormat:@"%.2f", 0.01]; //商品价格
-        
-        // NOTE: 参数编码格式
-        order.charset = @"utf-8";
-        
-        // NOTE: 支付接口名称
-        order.method = @"alipay.trade.app.pay";
-        
-        // NOTE: 仅支持JSON
-        order.format = @"json";
-        
-        // NOTE: 支付宝服务器主动通知商户服务器里指定的页面http/https路径。建议商户使用https
-        order.notify_url = @"http://www.jydsapp.com";
-        
-        // NOTE: sign_type设置
-        order.sign_type = @"RSA2";
-        
-        // NOTE: 当前时间点
-        NSDateFormatter* formatter = [NSDateFormatter new];
-        [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-        order.timestamp = [formatter stringFromDate:[NSDate date]];
-        
-        // NOTE: 支付版本
-        order.version = @"1.0";
-        
-        //将商品信息拼接成字符串
-        NSString *orderInfo = [order orderInfoEncoded:NO];
-        NSString *orderInfoEncoded = [order orderInfoEncoded:YES];
-        
-        // NOTE: 获取私钥并将商户信息签名，外部商户的加签过程请务必放在服务端，防止公私钥数据泄露；
-        //       需要遵循RSA签名规范，并将签名字符串base64编码和UrlEncode
-//        id<DataSigner> signer = CreateRSADataSigner(privateKey);
-//        NSString *signedString = [signer signString:orderInfo];
-        NSLog(@"加签前------------%@",orderInfo);
-        
-        [YHWebRequest YHWebRequestForPOST:@"http://www.zhongshuo.cn:8088/jyds/API/order_generateAPI" parameters:@{@"test":orderInfo} success:^(NSDictionary *json) {
-            if ([json[@"code"] isEqualToString:@"SUCCESS"]) {
-                // NOTE: 如果加签成功，则继续执行支付
-                if (json[@"sign"] != nil) {
-                    //应用注册scheme,在AliSDKDemo-Info.plist定义URL types
-                    NSString *appScheme = @"jydsapp";
-                    NSString *signedString = [json valueForKey:@"sign"];
-                    NSString *newSignedString =(__bridge_transfer  NSString*)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)signedString, NULL, (__bridge CFStringRef)@"!*'();:@&=+$,/?%#[]", kCFStringEncodingUTF8);
-                    // NOTE: 将签名成功字符串格式化为订单字符串,请严格按照该格式
-                    NSString *orderString = [NSString stringWithFormat:@"%@&sign=%@",
-                                             orderInfoEncoded, newSignedString];
-                    NSLog(@"加签后------------%@",orderString);
-                    // NOTE: 调用支付结果开始支付
-                    [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
-                        NSLog(@"reslut ======== %@",resultDic);
-                    }];
-                }else{
-                    [YHHud showWithMessage:@"交易失败"];
-                }
-            }else{
-                [YHHud showWithMessage:@"交易失败"];
+        [YHWebRequest YHWebRequestForPOST:@"http://www.zhongshuo.cn:8088/payAPI/API/order_generateAPI" parameters:nil success:^(NSDictionary *json) {
+            [YHHud dismiss];
+            NSMutableDictionary *jsonDic = [[NSMutableDictionary alloc] initWithDictionary:json];
+            NSArray* sortedKeyArray = [[jsonDic allKeys] sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+                return [obj1 compare:obj2];
+            }];
+            NSMutableArray *tmpArray = [NSMutableArray array];
+            for (NSString* key in sortedKeyArray) {
+                NSString *value = jsonDic[key];
+                value = (__bridge_transfer  NSString*)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)value, NULL, (__bridge CFStringRef)@"!*'();:@&=+$,/?%#[]", kCFStringEncodingUTF8 );
+                NSString* orderItem = [NSString stringWithFormat:@"%@=%@", key, value];
+                [tmpArray addObject:orderItem];
             }
+            NSString *orderString = [tmpArray componentsJoinedByString:@"&"];
+            //调起支付宝支付
+            [[AlipaySDK defaultService] payOrder:orderString fromScheme:@"jydsapp58327007" callback:^(NSDictionary *resultDic) {
+                NSLog(@"reslut ======== %@",resultDic);
+                [YHHud showWithMessage:resultDic[@"memo"]];
+            }];
         }];
     }else{
     //微信支付
-        [YHWebRequest YHWebRequestForPOST:@"http://www.zhongshuo.cn:8088/payAPI/API/wx_unifiedorderAPI" parameters:nil success:^(NSDictionary *json) {
-            if (![json[@"prepayId"] isEqualToString:@""]) {
-                PayReq *request = [[PayReq alloc] init];
-                request.partnerId = json[@"partnerId"];
-                request.prepayId = json[@"prepayId"];
-                request.package = json[@"package"];
-                request.nonceStr= json[@"nonceStr"];
-                request.timeStamp = [json[@"timestamp"] intValue];
-                request.sign = json[@"sign"];
-                [WXApi sendReq:request];
-            }
-        }];
+        if (![WXApi isWXAppInstalled]) {
+            [YHHud dismiss];
+            [YHHud showWithMessage:@" 请您先安装微信"];
+        }else{
+            [YHWebRequest YHWebRequestForPOST:@"http://www.zhongshuo.cn:8088/payAPI/API/wx_unifiedorderAPI" parameters:nil success:^(NSDictionary *json) {
+                [YHHud dismiss];
+                if (![json[@"prepayId"] isEqualToString:@""]) {
+                    PayReq *request = [[PayReq alloc] init];
+                    request.partnerId = json[@"partnerId"];
+                    request.prepayId = json[@"prepayId"];
+                    request.package = json[@"package"];
+                    request.nonceStr= json[@"nonceStr"];
+                    request.timeStamp = [json[@"timestamp"] intValue];
+                    request.sign = json[@"sign"];
+                     //调起微信支付
+                    [WXApi sendReq:request];
+                }
+            }];
+        }
     }
 }
 @end
